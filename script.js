c66dea0f127e0a62e9f7876e6d9cf7a777effb1e
Atharv Vyas <95745167+Mr-okey@users.noreply.github.com>
2022-12-17 21:51:06 +0530

    pop over added for copied link
    added pop over when link is copied
    added images to button
    added link image to button
    changed max-width of body
    centered body

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const fade = document.querySelector('.copiedtext');
+
 function copyToClipboard(element) {
   var $temp = $("<input>");
   $("body").append($temp);
   $temp.val("github.com/gamedevCloudy").select();
   document.execCommand("copy");
   $temp.remove();
+
+  // changed below
+  $(".copiedtext").addClass("fade")
 }
